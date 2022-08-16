dda1350169da32bbc321481cb483bc52994fbfb4
Taamir Ransome <taamir.ransome@sofwarellc.com>
2022-08-16 10:36:03 -0400

    Remove time variable

--- ruby/lib/studio_game.rb
+++ ruby/lib/studio_game.rb
@@ -6,10 +6,6 @@ def say_hello(name, health = 100)
   "I'm #{name.capitalize} with a health of #{health}."
 end
 
-current_time = Time.new
-formatted_time = current_time.strftime("%A %m/%d/%Y at %I:%M%p")
-puts "The game started on #{formatted_time}"
-
 puts "Players: \n\t#{name1}\n\t#{name2}\n\t#{name3}"
 puts "#{name1.capitalize} has a health of #{health1}."
 puts "#{name2.upcase} has a health of #{health2}."
