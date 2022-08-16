0e969ec8a68e2f243193c9a57a2ca9bc82aafdc2
Taamir Ransome <taamir.ransome@sofwarellc.com>
2022-08-16 10:16:20 -0400

    Add start time to game

--- ruby/lib/studio_game.rb
+++ ruby/lib/studio_game.rb
@@ -11,6 +11,10 @@ health2 = 125
 health3 = 100
 health4 = 90
 
+current_time = Time.new
+formatted_time = current_time.strftime("%A %m/%d/%Y at %I:%M%p")
+puts "The game started on #{formatted_time}"
+
 puts "Players: \n\t#{name1}\n\t#{name2}\n\t#{name3}"
 puts "#{name1.capitalize} has a health of #{health1}."
 puts "#{name2.upcase} has a health of #{health2}."
