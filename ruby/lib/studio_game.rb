3bda11956e525b8ce15fb256290e610507e8575d
Taamir Ransome <taamir.ransome@sofwarellc.com>
2022-08-16 10:37:24 -0400

    Display players using updated code

--- ruby/lib/studio_game.rb
+++ ruby/lib/studio_game.rb
@@ -10,3 +10,7 @@ def say_hello(name, health = 100)
   "I'm #{name.capitalize} with a health of #{health}."
 end
 
+puts say_hello('larry', 60)
+puts say_hello('curly', 60)
+puts say_hello('moe', 60)
+puts say_hello('shemp', 90)
