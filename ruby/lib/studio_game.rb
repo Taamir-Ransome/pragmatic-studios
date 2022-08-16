662ca7c460539520e1e0deac240033d333025512
Taamir Ransome <taamir.ransome@sofwarellc.com>
2022-08-16 10:36:21 -0400

    Create time method

--- ruby/lib/studio_game.rb
+++ ruby/lib/studio_game.rb
@@ -1,6 +1,10 @@
 # string literal comment
 # frozen string literal: true
 
+def time
+  current_time = Time.new
+  current_time.strftime("%I:%M:%S")
+end
 
 def say_hello(name, health = 100)
   "I'm #{name.capitalize} with a health of #{health}."
